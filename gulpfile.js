099675c07c3261e56294679480a06707cedca545
JaneLytv <jane.lytv@gmail.com>
2015-06-01 16:31:35 -0400

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require ('gulp-sass');
+var concat = require ('gulp-concat');
+var notify = require("gulp-notify");
+
+
+gulp.task('styles', function (){
+	return gulp.src('styles/**/*.scss')
+			.pipe(sass({
+			'sourcemap=none': true,
+			errLogToConsole: true
+				}))
+			.on("error", notify.onError(function (error) {
+			return "Message to the notifier: " + error.message;
+ 				}))
+			.pipe(concat('style.css'))
+			.pipe(gulp.dest('.'))
+			.pipe(notify('Sass done'));
+});
+
+gulp.task('default',function(){
+	gulp.watch('styles/**/*.scss',['styles'])
+});
